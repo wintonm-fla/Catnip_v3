aa28ad362eecc1a5dd4a01591ad6cc43c889ff44
wintonm-fla <wintonm@floridapanthers.com>
2025-08-05 13:55:48 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "catnip_v3",
+    version = "0.0.469",
+    author = "wintonm-fla",
+    author_email = "wintonm@floridapanthers.com",
+    description = "Facilitating repeatable internal code",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/wintonm-fla/Catnip_v3",
+    license = "MIT",
+    packages = find_packages(),
+    install_requires = requirements
+)
